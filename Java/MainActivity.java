ee17b4ad38584b1ed3bda9eaacdc27f00c97d648
HuiZhenChin <132343277+HuiZhenChin@users.noreply.github.com>
2023-09-20 22:26:53 +0800

    Create MainActivity.java

---
+++ Java/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Intent;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button loginButton = findViewById(R.id.loginBtn);
+
+
+        // Set click listeners for the plus and minus buttons
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+
+    }
+
+    public void openNewActivity(){
+        Intent intent = new Intent(this, NewActivity.class);
+        startActivity(intent);
+    }
+}
