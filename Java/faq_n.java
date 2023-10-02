e4d2e6edc2b0fee90dc6243ce4d2fe69117d980e
lian-yu-jia <133181629+lian-yu-jia@users.noreply.github.com>
2023-10-02 12:15:00 +0800

    Add files via upload

---
+++ Java/faq_n.java
@@ -0,0 +1,19 @@
+package com.example.project2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class faq_n extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+
+
+        };
+
+    }
