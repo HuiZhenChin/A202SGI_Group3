54d72f8bff2c560a2cf7ad925da4022997342fed
PehJiaXuan <133181953+PehJiaXuan@users.noreply.github.com>
2023-10-02 12:07:21 +0800

    Add files via upload

---
+++ Java/item_select_contact_us.java
@@ -0,0 +1,28 @@
+package com.example.faq;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class item_select_contact_us extends AppCompatActivity {
+    ImageButton imageButton_contact_us;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contact_us);
+
+        imageButton_contact_us = findViewById(R.id.imageButton_contact_us);
+
+        imageButton_contact_us.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(item_select_contact_us.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
