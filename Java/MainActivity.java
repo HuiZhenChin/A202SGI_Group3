778e3bc400a4a45e9f23e62da38f3b6d5d176623
uns3ri0us <115383851+uns3ri0us@users.noreply.github.com>
2023-10-06 23:16:09 +0800

    Update MainActivity.java

--- Java/MainActivity.java
+++ Java/MainActivity.java
@@ -1,69 +1,165 @@
-//Sign up page
-package com.example.project;
+<?xml version="1.0" encoding="utf-8"?>
+<RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:background="@drawable/background1"
+    tools:context=".MainActivity">
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.app.DatePickerDialog;
-import java.util.Calendar;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.content.Intent;
-import android.widget.Spinner;
-import android.widget.AdapterView;
+    <TextView
+        android:id="@+id/textView5"
+        android:layout_width="142dp"
+        android:layout_height="37dp"
+        android:layout_marginLeft="30dp"
+        android:layout_marginTop="460dp"
+        android:text="Password"
+        android:textSize="20sp" />
 
-public class MainActivity extends AppCompatActivity {
-    Button dateBtn;
-    TextView selectedDateView;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    <TextView
+        android:id="@+id/textView3"
+        android:layout_width="142dp"
+        android:layout_height="37dp"
+        android:layout_marginLeft="30dp"
+        android:layout_marginTop="360dp"
+        android:text="Date of Birth"
+        android:textSize="20sp" />
 
-        dateBtn = findViewById(R.id.dateInput);
-        selectedDateView = findViewById(R.id.selectedDate);
+    <TextView
+        android:id="@+id/textView4"
+        android:layout_width="142dp"
+        android:layout_height="37dp"
+        android:layout_marginLeft="30dp"
+        android:layout_marginTop="410dp"
+        android:text="Email"
+        android:textSize="20sp" />
 
-        dateBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    <TextView
+        android:id="@+id/textView6"
+        android:layout_width="142dp"
+        android:layout_height="60dp"
+        android:layout_marginLeft="30dp"
+        android:layout_marginTop="510dp"
+        android:text="Confirm Password"
+        android:textSize="20sp" />
 
-                final Calendar c = Calendar.getInstance();
 
-                int year = c.get(Calendar.YEAR);
-                int month = c.get(Calendar.MONTH);
-                int day = c.get(Calendar.DAY_OF_MONTH);
+    <TextView
+        android:id="@+id/textView"
+        android:layout_width="287dp"
+        android:layout_height="68dp"
+        android:layout_marginLeft="59dp"
+        android:layout_marginTop="25dp"
+        android:fontFamily="@font/varela"
+        android:text="Sign Up"
+        android:textAlignment="center"
+        android:textColor="@color/white"
+        android:textSize="50sp"
+        android:textStyle="bold" />
 
-                DatePickerDialog datePickerDialog = new DatePickerDialog(
-                        //passing context.
-                        MainActivity.this,
-                        new DatePickerDialog.OnDateSetListener() {
-                            @Override
-                            public void onDateSet(DatePicker view, int year,
-                                                  int monthOfYear, int dayOfMonth) {
-                                //  date to text view.
-                                selectedDateView.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+    <ImageView
+        android:id="@+id/imageView"
+        android:layout_width="170dp"
+        android:layout_height="170dp"
+        android:layout_marginLeft="120dp"
+        android:layout_marginTop="93dp"
+        app:srcCompat="@drawable/cat" />
 
-                            }
-                        },
+    <TextView
+        android:id="@+id/textView2"
+        android:layout_width="142dp"
+        android:layout_height="37dp"
+        android:layout_marginLeft="30dp"
+        android:layout_marginTop="310dp"
+        android:layout_marginBottom="100dp"
+        android:text="Username"
+        android:textSize="20sp" />
 
-                        year, month, day);
+    <EditText
+        android:id="@+id/username"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_marginLeft="170dp"
+        android:layout_marginTop="300dp"
+        android:ems="10"
+        android:hint="Username"
+        android:inputType="text"
+        android:minHeight="48dp" />
 
-                // display date picker dialog.
-                datePickerDialog.show();
-            }
-        });
-    }
+    <TextView
+        android:id="@+id/selectedDate"
+        android:layout_width="130dp"
+        android:layout_height="41dp"
+        android:layout_centerInParent="false"
+        android:layout_marginLeft="170dp"
+        android:layout_marginTop="350dp"
+        android:fontFamily="@font/varela"
+        android:background="@drawable/dateview_border"
+        android:focusable="true"
+        android:focusableInTouchMode="true"
+        android:text="test"
+        android:textAlignment="textStart"
+        android:textColor="@color/btn_color"
+        android:textSize="18sp" />
 
-    public void Signup(View view){
-        Intent intent = new Intent(this, MyAcc.class);
+    <Button
+        android:id="@+id/dateInput"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_centerInParent="false"
+        android:layout_centerVertical="false"
+        android:layout_marginLeft="300dp"
+        android:layout_marginTop="350dp"
+        android:background="@drawable/button"
+        android:hint="Choose Date"
+        android:textColorHint="@color/white"
+        android:textAllCaps="false" />
 
-        startActivity(intent);
+    <EditText
+        android:id="@+id/email"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_marginLeft="170dp"
+        android:layout_marginTop="400dp"
+        android:ems="10"
+        android:hint="Email"
+        android:inputType="textEmailAddress"
+        android:minHeight="48dp" />
 
-    }
-}
+    <EditText
+        android:id="@+id/pass"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_marginLeft="170dp"
+        android:layout_marginTop="450dp"
+        android:ems="10"
+        android:hint="Password"
+        android:inputType="textPassword"
+        android:minHeight="48dp" />
+
+    <EditText
+        android:id="@+id/confirm_pass"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_marginLeft="170dp"
+        android:layout_marginTop="515dp"
+        android:ems="10"
+        android:hint="Confirm Password"
+        android:inputType="textPassword"
+        android:minHeight="48dp" />
+
+
+    <Button
+        android:id="@+id/createTask"
+        android:layout_width="104sp"
+        android:layout_height="48sp"
+        android:layout_marginLeft="270dp"
+        android:layout_marginTop="600dp"
+        android:background="@drawable/button"
+        android:fontFamily="@font/varela"
+        android:textColor="@drawable/btn_text_color"
+        android:text="Sign Up"
+        android:textSize="16sp"
+        android:onClick="Signup"/>
+
+</RelativeLayout>
