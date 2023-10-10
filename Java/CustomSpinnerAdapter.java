8dbee4565fc5ad64cad1411827d3ba12fd156f5a
PehJiaXuan <133181953+PehJiaXuan@users.noreply.github.com>
2023-10-10 12:55:02 +0800

    Add files via upload

---
+++ Java/CustomSpinnerAdapter.java
@@ -0,0 +1,19 @@
+package com.example.calendarview;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class CustomSpinnerAdapter extends ArrayAdapter<String> {
+    public CustomSpinnerAdapter(Context context, int resource, String[] objects) {
+        super(context, resource, objects);
+    }
+
+    @Override
+    public boolean isEnabled(int position) {
+        // Disable the first item from Spinner (position 0) which acts as a hint
+        return position != 0;
+    }
+}
+
+
+
