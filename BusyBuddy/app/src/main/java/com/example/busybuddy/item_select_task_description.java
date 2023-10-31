8d4dd429c933c9030d7fcf26aef8185013bbfa66
PehJiaXuan <133181953+PehJiaXuan@users.noreply.github.com>
2023-10-31 13:02:37 +0800

    Add files via upload

---
+++ BusyBuddy/app/src/main/java/com/example/busybuddy/item_select_task_description.java
@@ -0,0 +1,30 @@
+package com.example.busybuddy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.busybuddy.R;
+
+public class item_select_task_description extends AppCompatActivity {
+    ImageButton imageButton_task_description;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.task_subpage);
+
+        imageButton_task_description = findViewById(R.id.imageButton_task_description);
+
+        imageButton_task_description.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(item_select_task_description.this, FaqMain.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
