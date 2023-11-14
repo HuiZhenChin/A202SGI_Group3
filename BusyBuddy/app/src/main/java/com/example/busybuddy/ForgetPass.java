40de288e16c4a7a41ae8e0a4975915b1966fa245
HuiZhenChin <132343277+HuiZhenChin@users.noreply.github.com>
2023-11-14 15:44:48 +0800

    Create ForgetPass.java

---
+++ BusyBuddy/app/src/main/java/com/example/busybuddy/ForgetPass.java
@@ -0,0 +1,12 @@
+package com.example.busybuddy;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
