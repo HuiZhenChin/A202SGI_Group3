b278eec44e2606f1a10257f925696d051a689cd9
lian-yu-jia <133181629+lian-yu-jia@users.noreply.github.com>
2023-11-14 21:32:36 +0800

    Update item_select_about_the_app.java

--- BusyBuddy/app/src/main/java/com/example/busybuddy/item_select_about_the_app.java
+++ BusyBuddy/app/src/main/java/com/example/busybuddy/item_select_about_the_app.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.busybuddy.R;
 
+// FAQ: About the App
 public class item_select_about_the_app extends AppCompatActivity {
     ImageButton imageButton_about_the_app;
 
@@ -19,10 +20,16 @@ public class item_select_about_the_app extends AppCompatActivity {
 
         imageButton_about_the_app = findViewById(R.id.imageButton_about_the_app);
 
+        // get username
+        Intent intentLogin = getIntent();
+        String usernameValue = intentLogin.getStringExtra("usernameValue");
+
+        // return to the FAQ Main Page
         imageButton_about_the_app.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
                 Intent intent = new Intent(item_select_about_the_app.this, FaqMain.class);
+                intent.putExtra("usernameValue", usernameValue);
                 startActivity(intent);
             }
         });
