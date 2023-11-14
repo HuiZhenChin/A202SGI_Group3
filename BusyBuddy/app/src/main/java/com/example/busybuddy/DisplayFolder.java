7c1f03d09f3565244cc006b1dfee9dd40d444466
HuiZhenChin <132343277+HuiZhenChin@users.noreply.github.com>
2023-11-14 15:41:57 +0800

    Create DisplayFolder.java

---
+++ BusyBuddy/app/src/main/java/com/example/busybuddy/DisplayFolder.java
@@ -0,0 +1,15 @@
+package com.example.busybuddy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.busybuddy.R;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
