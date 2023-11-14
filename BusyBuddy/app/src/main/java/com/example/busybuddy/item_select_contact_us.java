38ac8679143cf03822a3eb49cde4835e3d8e265c
lian-yu-jia <133181629+lian-yu-jia@users.noreply.github.com>
2023-11-14 21:32:59 +0800

    Update item_select_contact_us.java

--- BusyBuddy/app/src/main/java/com/example/busybuddy/item_select_contact_us.java
+++ BusyBuddy/app/src/main/java/com/example/busybuddy/item_select_contact_us.java
@@ -9,7 +9,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.busybuddy.R;
 
+// FAQ: Contact Us
 public class item_select_contact_us extends AppCompatActivity {
+    String usernameValue = "";
     ImageButton imageButton_contact_us;
 
     @Override
@@ -19,10 +21,17 @@ public class item_select_contact_us extends AppCompatActivity {
 
         imageButton_contact_us = findViewById(R.id.imageButton_contact_us);
 
+        // get username
+        Intent intentLogin = getIntent();
+        if (usernameValue!= null) {
+            usernameValue = intentLogin.getStringExtra("usernameValue");
+        }
+        // return to the FAQ Main Page
         imageButton_contact_us.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
                 Intent intent = new Intent(item_select_contact_us.this, FaqMain.class);
+                intent.putExtra("usernameValue", usernameValue);
                 startActivity(intent);
             }
         });
