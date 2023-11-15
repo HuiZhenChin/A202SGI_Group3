612d4e06dcd6b92a7bdc75ec213dea3e1bf25425
PehJiaXuan <133181953+PehJiaXuan@users.noreply.github.com>
2023-11-15 17:03:17 +0800

    Update CalendarViewHolder.java

--- BusyBuddy/app/src/main/java/com/example/busybuddy/CalendarViewHolder.java
+++ BusyBuddy/app/src/main/java/com/example/busybuddy/CalendarViewHolder.java
@@ -9,12 +9,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
+// Calendar
 public class CalendarViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
 {
     private final ArrayList<LocalDate> days;
     public final View parentView;
     public final TextView dayOfMonth;
     private final CalendarAdapter.OnItemListener onItemListener;
+
     public CalendarViewHolder(@NonNull View itemView, CalendarAdapter.OnItemListener onItemListener, ArrayList<LocalDate> days)
     {
         super(itemView);
@@ -23,6 +25,7 @@ public class CalendarViewHolder extends RecyclerView.ViewHolder implements View.
         this.onItemListener = onItemListener;
         itemView.setOnClickListener(this);
         this.days = days;
+
     }
 
     @Override
