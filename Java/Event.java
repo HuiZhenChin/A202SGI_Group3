e7e5b7db674a9a36ada93dbe5ef4c5ca7b0d7167
PehJiaXuan <133181953+PehJiaXuan@users.noreply.github.com>
2023-11-15 16:34:05 +0800

    Update Event.java

--- Java/Event.java
+++ Java/Event.java
@@ -1,13 +1,15 @@
-package com.example.calendarview;
+package com.example.busybuddy;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 
+// Event class (Calendar Event)
 public class Event
 {
     public static ArrayList<Event> eventsList = new ArrayList<>();
 
+    // add event to the list
     public static ArrayList<Event> eventsForDate(LocalDate date)
     {
         ArrayList<Event> events = new ArrayList<>();
@@ -26,19 +28,27 @@ public class Event
     private LocalDate date;
     private LocalTime time;
 
+    // check whether the list has event or not
     public static boolean hasEventsForDate(LocalDate date) {
+        if (date == null) {
+            return false; // or handle the case where date is null
+        }
+
         for (Event event : eventsList) {
-            if(event.getDate().equals(date))
+            LocalDate eventDate = event.getDate();
+            if (eventDate != null && eventDate.equals(date)) {
                 return true;
+            }
         }
         return false;
     }
 
+
     public Event(String name, LocalDate date, LocalTime time)
     {
-        this.name = name;
-        this.date = date;
-        this.time = time;
+        this.name = name;  // event title
+        this.date = date;  // event date
+        this.time = time;  // event created time
     }
 
     public String getName()
