1216b29d1a0d1a397cdc401d1bec6f8752c21382
PehJiaXuan <133181953+PehJiaXuan@users.noreply.github.com>
2023-11-15 17:04:50 +0800

    Update item_select_task_delete.java

--- BusyBuddy/app/src/main/java/com/example/busybuddy/item_select_task_delete.java
+++ BusyBuddy/app/src/main/java/com/example/busybuddy/item_select_task_delete.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.busybuddy.R;
 
+// FAQ: Delete Task
 public class item_select_task_delete extends AppCompatActivity {
     ImageButton imageButton_task_delete;
 
@@ -19,10 +20,16 @@ public class item_select_task_delete extends AppCompatActivity {
 
         imageButton_task_delete = findViewById(R.id.imageButton_task_delete);
 
+        // get username
+        Intent intentLogin = getIntent();
+        String usernameValue = intentLogin.getStringExtra("usernameValue");
+
+        // return to the FAQ Main Page
         imageButton_task_delete.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
                 Intent intent = new Intent(item_select_task_delete.this, FaqMain.class);
+                intent.putExtra("usernameValue", usernameValue);
                 startActivity(intent);
             }
         });
