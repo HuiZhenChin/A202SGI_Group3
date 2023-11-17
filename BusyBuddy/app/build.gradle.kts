08c8117ea34195a6f60d064a94b3faca1254bc82
HuiZhenChin <132343277+HuiZhenChin@users.noreply.github.com>
2023-11-17 20:16:13 +0800

    Update build.gradle.kts

--- BusyBuddy/app/build.gradle.kts
+++ BusyBuddy/app/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.test5"
+    namespace = "com.example.busybuddy"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.test5"
+        applicationId = "com.example.busybuddy"
         minSdk = 28
         targetSdk = 34
         versionCode = 1
